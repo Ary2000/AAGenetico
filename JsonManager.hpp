2da4af5e201a98aa1c9fe6dbc82faa6b6c707272
Sebascam1498 <55771436+Sebascam1498@users.noreply.github.com>
2021-01-07 12:10:54 -0600

    update

--- JsonManager.hpp
+++ JsonManager.hpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector> 
+#include <vector>
+#include "rxcpp/rx.hpp"
 
 using json = nlohmann::json;
 using namespace std;
@@ -63,38 +64,9 @@ std::vector<double> parserDatos(std::string Tramo){
     return Datos;
 }
 
-std::vector<std::vector<double>> JsonReader(std::string NombreJson){
-
-    json bufferJson;
-    std::string bufferString;
-    std::vector<std::vector<double>> DatosTramos={};
-
-    std::ifstream file(NombreJson);
-    json j = json::parse(file);
-
-    for (auto it = j["Tramos"].begin(); it != j["Tramos"].end(); ++it){
-    bufferJson = it.value();
-    bufferString=bufferJson.dump();
-    DatosTramos.push_back(parserDatos(bufferString));
-    std::cout << it.key() << " | " << it.value() << "\n";
-    }
-
-    for (int i = 0; i < DatosTramos.size(); i++){
-       cout<<"Tramo "<<i<<endl;
-       for (int z = 0; z < DatosTramos[i].size(); z++){
-        cout<< DatosTramos[i][z]<< " ";
-        }
-        cout<<endl;
-    }
-    return DatosTramos;
-
-}
-
 void JsonWritter(std::vector<std::vector<double>> DatosTramos){
-
     json jsonfile;
     std::string NumTramo;
-
     for (int indice = 0; indice < DatosTramos.size(); indice++)
     {
         NumTramo = "tramo"+to_string(indice);
@@ -104,7 +76,49 @@ void JsonWritter(std::vector<std::vector<double>> DatosTramos){
         jsonfile["Tramos"][NumTramo]["KmEnd"] = DatosTramos[indice][3];
         jsonfile["Tramos"][NumTramo]["KmStart"] = DatosTramos[indice][4];
     }
+    
+    
 
     std::ofstream file("MapaSensor.json");
     file << jsonfile.dump(1);
 }
+
+std::vector<std::vector<double>> JsonReader(std::string NombreJson, std::vector<std::vector<double>>* PointerVectorGlobal){
+
+    json bufferJson;
+    std::string bufferString;
+    std::vector<std::vector<double>> &DatosTramos = *PointerVectorGlobal;
+    std::vector<std::vector<double>> VectorRecorrido = {};
+    std::vector<double> bufferDatos = {};
+
+    std::ifstream file(NombreJson);
+    json j = json::parse(file);
+
+    for (auto it = j["Tramos"].begin(); it != j["Tramos"].end(); ++it){
+    bufferJson = it.value();
+    bufferString=bufferJson.dump();
+    //cout << "Waiting..." << endl;
+    auto values = rxcpp::observable<>::timer<>(std::chrono::seconds(10));
+    values.subscribe([](int v) {});
+    bufferDatos= parserDatos(bufferString);
+    DatosTramos.push_back(bufferDatos);
+    VectorRecorrido.push_back(bufferDatos);
+    JsonWritter(VectorRecorrido);
+    //cout << it.key() << " | " << it.value() << "\n";
+    }
+    /*
+    //info de los tramos
+    
+    for (int i = 0; i < DatosTramos.size(); i++){
+       cout<<"Tramo "<<i<<endl;
+       for (int z = 0; z < DatosTramos[i].size(); z++){
+        cout<< DatosTramos[i][z]<< " ";
+        }
+        cout<<endl;
+    }
+    */
+   
+    return DatosTramos;
+
+}
+
--- prueba.cpp
+++ prueba.cpp
@@ -1,21 +1,38 @@
-#include "rxcpp/rx.hpp"
+
 #include "JsonManager.hpp"
 #include <thread>  
 
 using namespace std;
-std::vector<std::vector<double>> a;
+std::vector<std::vector<double>>* v = new vector<std::vector<double>>();
+std::vector<std::vector<double>> &vr = *v;
+
+void printVector(std::vector<double> vector){
+    for (int i = 0; i < vector.size(); i++)
+    {
+        cout<<vector[i];
+    }
+    
+}
 
 int main()
 {
-    
-    cout << "Waiting..." << endl;
-    auto values = rxcpp::observable<>::timer<>(std::chrono::seconds(10));
-    values.subscribe([](int v) {cout << "Called after 1s." << endl;});
-    std::thread first (JsonReader,"Mapa.json");
-    a = first;
+    int flag=0;
+    std::thread first (JsonReader,"Mapa.json",v);
+    while(flag<2){
+        if (vr.size()!=0)
+        {
+            std::thread second (printVector,vr[0]);
+            second.join();
+            cout<<endl;
+            vr.erase(vr.begin());
+            flag++;
+                
+        }
+    }
 
-    cout<<endl<<a[0][1];
+    first.join();
     
+    cout<<endl<<"Finish";
     return 0;
 }
 
